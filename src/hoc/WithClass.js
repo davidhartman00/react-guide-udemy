fcf6756bab0f3e7f3002c20e1fe014d857fc8575
davidhartman00 <davidhartman00@gmail.com>
2019-06-21 18:40:37 -0700

    file name change

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -4,8 +4,7 @@ const withClass = (WrappedComponent, className) => {
     return props =>
        ( <div className={className}>
             <WrappedComponent {...props}/>
-        </div>)
-    
+        </div>)    
 }
 
 export default withClass
